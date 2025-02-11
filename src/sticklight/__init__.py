90b286aa92b8da37df82d4fcf1948b874436146c
Gilad Barnea <giladbrn@gmail.com>
2025-02-11 12:28:34 +0200

    Add `acapture()` and aiohttp dep with speedups

--- src/sticklight/__init__.py
+++ src/sticklight/__init__.py
@@ -1,1 +1,1 @@
-from .capture import capture as capture
+from .capture import acapture as acapture, capture as capture
--- src/sticklight/capture.py
+++ src/sticklight/capture.py
@@ -1,7 +1,12 @@
+import typing
+
 import requests
 
 from .auth import resolve_sticklight_api_key
 from .consts import STICKLIGHT_API_BASE_URL
+
+if typing.TYPE_CHECKING:
+    import aiohttp
 
 
 def capture(
@@ -29,4 +34,37 @@ def capture(
     return response
 
 
-__all__ = ["capture"]
+async def acapture(
+    data: dict, *, sticklight_api_key: str | None = None
+) -> "aiohttp.ClientResponse":
+    """
+    Publish an event to Sticklight API asynchronously.
+
+    Args:
+        data (dict): A free-form dictionary of data to publish.
+        sticklight_api_key (str, optional): The Sticklight API key to use. Defaults to None.
+            Either pass it to the function or set the STICKLIGHT_API_KEY environment variable.
+
+    Returns:
+        aiohttp.ClientResponse: The response from the Sticklight API.
+    """
+    import aiohttp
+
+    sticklight_api_key = resolve_sticklight_api_key(sticklight_api_key)
+    async with (
+        aiohttp.ClientSession() as session,
+        session.post(
+            f"{STICKLIGHT_API_BASE_URL}/events-collect/v1/events",
+            json=[data],
+            headers={
+                "accept": "application/json",
+                "x-api-key": sticklight_api_key,
+            },
+            timeout=5,
+        ) as response,
+    ):
+        response.raise_for_status()
+        return response
+
+
+__all__ = ["acapture", "capture"]
